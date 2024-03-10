ee6f704ac7801378fb1918513437ae302e1278c5
Jeff Handley <jeffhandley@users.noreply.github.com>
2024-03-09 20:01:07 -0800

    Support light and dark mode icons with detection (#3)

--- content-script.js
+++ content-script.js
@@ -1,12 +1,22 @@
-chrome.runtime.onMessage.addListener(({ url, text }) => {
-    const copyToClipboard = (event) => {
-        event.clipboardData.setData("text/plain", text);
-        event.clipboardData.setData("text/html", `<a href="${url}">${text}</a>`);
+chrome.runtime.onMessage.addListener(({ type, url, text }) => {
+    if (type === "copy") {
+        const copyToClipboard = (event) => {
+            event.clipboardData.setData("text/plain", text);
+            event.clipboardData.setData("text/html", `<a href="${url}">${text}</a>`);
 
-        event.preventDefault();
-    };
+            event.preventDefault();
+        };
 
-    document.addEventListener("copy", copyToClipboard);
-    document.execCommand("copy");
-    document.removeEventListener("copy", copyToClipboard);
+        document.addEventListener("copy", copyToClipboard);
+        document.execCommand("copy");
+        document.removeEventListener("copy", copyToClipboard);
+    }
 });
+
+function detectDarkMode() {
+    const darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    chrome.runtime.sendMessage({ type: "darkMode", darkMode });
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', detectDarkMode);
+detectDarkMode();
